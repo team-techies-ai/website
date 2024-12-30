3f84fa84aea37de898b45dad667e58c36c9ef4a1
praneeth338 <muppasanipraneeth338@gmail.com>
2024-12-30 09:10:57 +0530

    added the all profile

--- Frontend/src/team/Team.jsx
+++ Frontend/src/team/Team.jsx
@@ -1,18 +1,22 @@
-import Mainprofile from "../components/Mainprofile"
-import Profile from "../components/Profile"
+import Profile from "./Profile";
+import Mainprofile from "./Mainprofile";
+import Allprofiles from "./Allprofiles";
 
 const Team = () => {
   return (
-    <div className="bg-[#0b0314] h-screen  text-white mt-32">
-      <h1 className="text-6xl font-medium text-center m-4"> Meet our Team</h1>
+    <div className="bg-[#0b0314] min-h-screen text-white mt-32 flex flex-col">
+      <h1 className="text-6xl font-medium text-center m-4">Meet our Team</h1>
       <Mainprofile />
-     <div className="flex">
-     <Profile/>
-      <Profile/>
-
-     </div>
+      <div className="flex justify-center">
+        <Profile />
+        <Profile />
+      </div>
+      <div className="mt-auto">
+        <Allprofiles />
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
