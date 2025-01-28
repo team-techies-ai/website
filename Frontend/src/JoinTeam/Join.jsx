f89603bbfa78620b6f9d0efe82160028759df4bb
kummari-pavan <kpavan892002@gmail.com>
2025-01-29 00:40:13 +0530

    [PAVAN] Updated : conatct and nav components

--- Frontend/src/JoinTeam/Join.jsx
+++ Frontend/src/JoinTeam/Join.jsx
@@ -1,130 +1,13 @@
-// import { useForm } from "react-hook-form";
-// import { JoinTeam } from "../utils/api";
-
-// const Join = () => {
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm();
-
-//   const onSubmit = async (data) => {
-//     const formData = new FormData();
-//     formData.append("name", data.name);
-//     formData.append("email", data.email);
-//     formData.append("contact", data.contact);
-//     formData.append("reason", data.reason);
-//     formData.append("resume", data.resume[0]);
-
-//     JoinTeam(formData)
-//       .then((response) => {
-//         alert(response.data);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         alert("Error submitting form!");
-//       });
-//   };
-
-//   return (
-//     <div className="bg-[#0b0314] p-8 rounded-lg shadow-lg w-full max-w-md mt-32 mx-auto text-black border border-[#8154bb] ">
-//       <h1 className="text-2xl font-bold mb-6 text-center bg-gradient-to-r from-[#7203FF] via-[#ffffff] to-[#2D0C57] bg-clip-text text-transparent">
-//         Join Team Techies
-//       </h1>
-
-//       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-//         <div>
-//           <label className="block text-sm  text-white font-medium mb-2">Name</label>
-//           <input
-//             type="text"
-//             {...register("name", { required: "Name is required" })}
-//             className="w-full p-3 rounded  focus:ring-2 focus:ring-[#7203FF] outline-none"
-//           />
-//           {errors.name && (
-//             <p className="text-[#7203FF] text-sm mt-1">{errors.name.message}</p>
-//           )}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm text-white font-medium mb-2">Email</label>
-//           <input
-//             type="email"
-//             {...register("email", {
-//               required: "Email is required",
-//               pattern: {
-//                 value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-//                 message: "Invalid email address",
-//               },
-//             })}
-//             className="w-full p-3 rounded  text-black focus:ring-2 focus:ring-[#7203FF] outline-none"
-//           />
-//           {errors.email && (
-//             <p className="text-[#7203FF] text-sm mt-1">{errors.email.message}</p>
-//           )}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm text-white font-medium mb-2">Contact</label>
-//           <input
-//             type="tel"
-//             {...register("contact", {
-//               required: "Contact is required",
-//               pattern: {
-//                 value: /^[0-9]{10}$/,
-//                 message: "Invalid contact number",
-//               },
-//             })}
-//             className="w-full p-3 rounded   text-black  focus:ring-2 focus:ring-[#7203FF] outline-none"
-//           />
-//           {errors.contact && (
-//             <p className="text-[#7203FF] text-sm mt-1">{errors.contact.message}</p>
-//           )}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm text-white font-medium mb-2">Resume</label>
-//           <input
-//             type="file"
-//             {...register("resume", { required: "Resume is required" })}
-//             className="w-full p-3 bg-white  text-black  rounded focus:ring-2 focus:ring-[#7203FF] outline-none"
-//           />
-//           {errors.resume && (
-//             <p className="text-[#7203FF] text-sm mt-1">{errors.resume.message}</p>
-//           )}
-//         </div>
-
-//         <div>
-//           <label className="block text-sm text-white font-medium mb-2">Why do you want to join?</label>
-//           <textarea
-//             {...register("reason", { required: "This field is required" })}
-//             className="w-full p-3 rounded   text-black   focus:ring-2 focus:ring-[#7203FF] outline-none"
-//             rows="4"
-//           ></textarea>
-//           {errors.reason && (
-//             <p className="text-[#7203FF] text-sm mt-1">{errors.reason.message}</p>
-//           )}
-//         </div>
-
-//         <button
-//           type="submit"
-//           className="w-full p-3   bg-[#7203FF]  text-white  font-bold rounded hover:bg-[#7654a1] transition"
-//         >
-//           Submit
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Join;
-
-import React from "react";
+import React ,{ useState } from "react";
 import { useForm } from "react-hook-form";
 import { JoinTeam } from "../utils/api";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {Upload ,File, X} from "lucide-react";
 
 const Join = () => {
+  const [coverLetterFile, setCoverLetterFile] = useState(null)
+  const [resumeFile, setResumeFile] = useState(null)
   const {
     register,
     handleSubmit,
@@ -158,10 +41,13 @@ const Join = () => {
     formData.append("name", data.name);
     formData.append("email", data.email);
     formData.append("contact", data.contact);
-    formData.append("resume", data.resume[0]);
-    formData.append("coverLetter", data.coverLetter[0]);
+    formData.append("resume", resumeFile);
+    formData.append("coverLetter", coverLetterFile);
+
+    console.log(formData,"----------------------------")
 
     JoinTeam(formData)
+      
       .then((response) => {
         notifySuccess();
       })
@@ -170,6 +56,17 @@ const Join = () => {
         notifyError();
       });
   };
+  const handleFileChange = (e, setFile) => {
+    if (e.target.files[0]) {
+      setFile(e.target.files[0])
+    }
+  }
+
+  const handleRemoveFile = (setFile) => {
+    setFile(null)
+  }
+
+
 
   return (
     <div className="bg-[#0b0314] p-8 rounded-lg shadow-lg w-full max-w-md mt-32 mx-auto text-black border border-[#8154bb] ">
@@ -226,42 +123,74 @@ const Join = () => {
           )}
         </div>
 
-        <div>
-          <input
-            type="file"
-            placeholder="Enter your Resume"
-            {...register("resume", { required: "Resume is required" })}
-            className="w-full p-3 bg-[#0b0314] text-white border rounded-md outline-none relative z-10 placeholder-gray-500"
-          />
-          {errors.resume && (
-            <p className="text-[#7203FF] text-sm mt-1">{errors.resume.message}</p>
-          )}
+        <div className="mb-4">
+          <div className="border border-dashed rounded-lg p-2">
+            <input
+              id="resume-upload"
+              type="file"
+              className="hidden"
+              {...register("resume", { required: "Resume is required" })}
+              onChange={(e) => handleFileChange(e, setResumeFile)}
+            />
+            {!resumeFile ? (
+              <label htmlFor="resume-upload" className="flex flex-col items-center justify-center h-32 cursor-pointer">
+                <Upload className="w-10 h-10 text-gray-400 mb-2" />
+                <span className="text-sm text-gray-400">Drag & drop or click to upload resume</span>
+              </label>
+            ) : (
+              <div className="flex items-center justify-between p-2">
+                <div className="flex items-center">
+                  <File className="w-8 h-8 text-gray-400 mr-2" />
+                  <span className="text-sm text-gray-400">{resumeFile.name}</span>
+                </div>
+                <button
+                  type="button"
+                  onClick={() => handleRemoveFile(setResumeFile)}
+                  className="text-gray-400 hover:text-gray-600"
+                >
+                  <X className="w-5 h-5" />
+                </button>
+              </div>
+            )}
+          </div>
+          {errors.resume && <p className="text-[#7203FF] text-sm mt-1">{errors.resume.message}</p>}
         </div>
 
-        {/* <div>
-          <label className="block text-sm text-white font-medium mb-2">Why do you want to join?</label>
-          <textarea
-            {...register("reason", { required: "This field is required" })}
-            className="w-full p-3 rounded focus:ring-2 focus:ring-[#7203FF] outline-none"
-            rows="4"
-          ></textarea>
-          {errors.reason && (
-            <p className="text-[#7203FF] text-sm mt-1">{errors.reason.message}</p>
-          )}
-        </div> */}
-
-        <div>
-          <input
-            type="file"
-            placeholder="Enter Cover Letter"
-            {...register("coverLetter")}
-            className="w-full p-3 bg-[#0b0314] text-white border rounded-md outline-none relative z-10 placeholder-gray-500"
-          />
-          {errors.coverLetter && (
-            <p className="text-[#7203FF] text-sm mt-1">{errors.coverLetter.message}</p>
-          )}
+        <div className="mb-4">
+          <div className="border border-dashed rounded-lg p-2">
+            <input
+              id="cover-letter-upload"
+              type="file"
+              className="hidden"
+              {...register("coverLetter")}
+              onChange={(e) => handleFileChange(e, setCoverLetterFile)}
+            />
+            {!coverLetterFile ? (
+              <label
+                htmlFor="cover-letter-upload"
+                className="flex flex-col items-center justify-center h-32 cursor-pointer"
+              >
+                <Upload className="w-10 h-10 text-gray-400 mb-2" />
+                <span className="text-sm text-gray-400">Drag & drop or click to upload cover letter(optional)</span>
+              </label>
+            ) : (
+              <div className="flex items-center justify-between p-2">
+                <div className="flex items-center">
+                  <File className="w-8 h-8 text-gray-400 mr-2" />
+                  <span className="text-sm text-gray-400">{coverLetterFile.name}</span>
+                </div>
+                <button
+                  type="button"
+                  onClick={() => handleRemoveFile(setCoverLetterFile)}
+                  className="text-gray-400 hover:text-gray-600"
+                >
+                  <X className="w-5 h-5" />
+                </button>
+              </div>
+            )}
+          </div>
+          {errors.coverLetter && <p className="text-[#7203FF] text-sm mt-1">{errors.coverLetter.message}</p>}
         </div>
-
 
         <button
           type="submit"
