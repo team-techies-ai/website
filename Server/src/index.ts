6ce958821f2aa1c3073fa31c8975e27e416586a0
praneeth <141117659+Muppasanipraneeth@users.noreply.github.com>
2025-02-23 21:15:43 +0530

    Update CORS configuration to restrict origins and specify allowed methods and headers

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 import connectDB from "./Config/db.config";
 import routes from "./Routes";
 import cors from "cors";
-dotenv.config();
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 9001;
-app.use(cors({origin:"*"}));
+
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use("/api", routes);
