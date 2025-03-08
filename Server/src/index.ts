f8e7b8a65f6d94178b4fb7f05d3037b172a9a1a9
Team Techies <jointeamtechies@gmail.com>
2025-03-09 01:03:09 +0530

    Update index.ts

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 9001;
 
 app.use(cors({
-  origin: "https://teamtechies.onrender.com/", 
+  origin: "https://teamtechiesbd.onrender.com/", 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
