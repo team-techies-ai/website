a19f093eb8cc94542f594719fa6c93888c3fcb2a
Team Techies <jointeamtechies@gmail.com>
2025-03-09 01:15:16 +0530

    Update index.ts

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -6,10 +6,10 @@ import cors from "cors";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://teamtechiesbd.onrender.com/", 
+  origin: "https://teamtechies.onrender.com/", 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
